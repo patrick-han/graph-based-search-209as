ed03886141b50989630c9ef752c706993c0d44d7
vishnu <devarakonda.vishnu5@gmail.com>
2021-10-26 16:40:32 -0700

    RRT fix

--- RRT.py
+++ RRT.py
@@ -4,29 +4,28 @@ from Tree import TreeNode
 import numpy as np
 
 class RRT:
-    def __init__(self, nearest: function) -> None:
+    def __init__(self) -> None:
         """
         Initialize an RRT object
-        args:
-            collision: Function with params (state, obstacles). Returns true
-            if state collides with any obstacles else False
-            nearest: Function with params (state, [List of TreeNodes]). Returns
-            the nearest/best node to state.
         """
-        self._nearest = nearest
 
     def get_closest(self, node_list, state):
+        """
+        args:
+            node_list: List of TreeNodes.
+            state: numpy array. The state to check distance against.
+        """
         minDist = float('inf')
-        return_state = None
+        return_node = None
         for node in node_list:
             node_state = node.val
             distance = np.linalg.norm(state - node_state, 2)
             if distance < minDist:
                 minDist = distance
-                return_state = node_state
-        return return_state
+                return_node = node
+        return return_node
 
-    def run(self, start_node, obstacles : list, sampler: function, drive: function, K = 100, step_count=100):
+    def run(self, start_node, obstacles : list, sampler: function, limit = 100, step_count=100):
         """
         Function run RRT
         args:
@@ -34,19 +33,20 @@ class RRT:
             obstacles: List. Obstacles that are in the space.
             sampler: Function with params(state). Determines how to sample a
             random point from the space.
-            drive: Function with params(state1, state2). Function returns a new
-            state when driving from state1 to state2.
+            limit: Number of RRT iterations to run.
+            step_count: Number of steps to make between randomly sampled point
+              and closest point in RRT graph.
         """
         head = TreeNode(start_node)
         G = [ head ]
-        while K:
+        while limit:
             new_state = sampler()
-            closest_state = self.get_closest(G, new_state)
+            closest_node = self.get_closest(G, new_state)
             for obs in obstacles:
-                new_state = obs.findMaxTrajectory(closest_state, new_state, step_count)
+                new_state = obs.findMaxTrajectory(
+                    closest_node.val, new_state, step_count)
             new_node = TreeNode(new_state)
-            nearest_node = self._nearest(new_state, G)
-            nearest_node._nodes.append(new_node)
+            closest_node.append(new_node)
             G.append(new_node)
-            K -= 1
+            limit -= 1
         return head
