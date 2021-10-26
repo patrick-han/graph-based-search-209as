9f0d9823db6a5983bc4ee4d6f1076ee7a0d705c5
vishnu <devarakonda.vishnu5@gmail.com>
2021-10-26 16:28:29 -0700

    normal RRT implemented

--- RRT.py
+++ RRT.py
@@ -1,9 +1,10 @@
 # Implements RRT
 # By: Vishnu Devarakonda
 from Tree import TreeNode
+import numpy as np
 
 class RRT:
-    def __init__(self, collision : function, nearest: function) -> None:
+    def __init__(self, nearest: function) -> None:
         """
         Initialize an RRT object
         args:
@@ -12,10 +13,20 @@ class RRT:
             nearest: Function with params (state, [List of TreeNodes]). Returns
             the nearest/best node to state.
         """
-        self._collision = collision
         self._nearest = nearest
 
-    def run(self, start_node, obstacles : list, sampler: function, K = 100):
+    def get_closest(self, node_list, state):
+        minDist = float('inf')
+        return_state = None
+        for node in node_list:
+            node_state = node.val
+            distance = np.linalg.norm(state - node_state, 2)
+            if distance < minDist:
+                minDist = distance
+                return_state = node_state
+        return return_state
+
+    def run(self, start_node, obstacles : list, sampler: function, drive: function, K = 100, step_count=100):
         """
         Function run RRT
         args:
@@ -23,15 +34,19 @@ class RRT:
             obstacles: List. Obstacles that are in the space.
             sampler: Function with params(state). Determines how to sample a
             random point from the space.
+            drive: Function with params(state1, state2). Function returns a new
+            state when driving from state1 to state2.
         """
         head = TreeNode(start_node)
         G = [ head ]
         while K:
             new_state = sampler()
-            if not self._collision(new_state, obstacles):
-                new_node = TreeNode(new_state)
-                nearest_node = self._nearest(new_state, G)
-                nearest_node._nodes.append(new_node)
-                G.append(new_node)
+            closest_state = self.get_closest(G, new_state)
+            for obs in obstacles:
+                new_state = obs.findMaxTrajectory(closest_state, new_state, step_count)
+            new_node = TreeNode(new_state)
+            nearest_node = self._nearest(new_state, G)
+            nearest_node._nodes.append(new_node)
+            G.append(new_node)
             K -= 1
         return head
